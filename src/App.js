9a0e5edc20275b97b50bb673e230bf0fcd346d82
adlyrfu <adly19222@gmail.com>
2022-10-21 17:48:12 +0700

    fix: files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Tugas from './project-reactJS/Tugas';
+
+const App = () => {
+  return (
+    <div>
+      <Tugas/>
+    </div>
+  )
+}
+
+export default App
